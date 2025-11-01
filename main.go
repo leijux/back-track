7ab0605acb9c1aaf90ccc00abf4682203f09fc9f
leiju <leijuxx@outlook.com>
2025-11-01 14:29:43 +0800

    移除未使用的导入和冗余代码，简化主程序结构

--- main.go
+++ main.go
@@ -17,21 +17,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
-	"os/exec"
 	"runtime/debug"
 
 	"github.com/spf13/cobra"
-)
-
-const (
-	// 备份数据目录名称
-	dataDirName = "data"
-
-	// 保留最新的备份数量
-	retainBackupCount = 3
 )
 
 var rootCmd = &cobra.Command{
@@ -44,30 +34,10 @@ func init() {
 
 	debugInfo, _ := debug.ReadBuildInfo()
 	rootCmd.Version = debugInfo.Main.Version
-
-	rootCmd.AddCommand(backupCmd, restoreCmd)
 }
 
 func main() {
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
-}
-
-func checkRoot(cmd *cobra.Command, args []string) error {
-	if os.Geteuid() != 0 {
-		return fmt.Errorf("请以root权限运行")
-	}
-	return nil
-}
-
-// runCommand 执行系统命令并返回结果
-func runCommand(name string, args ...string) (string, error) {
-	cmd := exec.Command(name, args...)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		return "", fmt.Errorf("命令执行失败 (%s %v): %w, 输出: %s",
-			name, args, err, string(output))
-	}
-	return string(output), nil
 }
